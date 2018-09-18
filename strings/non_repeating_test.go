9b4c669d16d4868c6a40332e53c7a2a3dbe89b85
Edison Lumbantoruan <Edison.Lumbantoruan@gmail.com>
2018-09-17 21:47:36 -0700

    add more unit test

--- strings/non_repeating_test.go
+++ strings/non_repeating_test.go
@@ -26,6 +26,11 @@ func TestNonRepeatingChar(t *testing.T) {
 			args: args{s: "aabb"},
 			want: ' ',
 		},
+		{
+			name: "Test4",
+			args: args{s: "aabbd"},
+			want: 'd',
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
